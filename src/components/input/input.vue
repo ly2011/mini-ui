e4974b99e321cd20d6e5360ae0c1a0a6049a7e0b
ly2011 <1518550424@qq.com>
2019-06-04 09:04:54 +0800

    <feat>: Input组件更新

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,9 +1,26 @@
 <template>
-  <input type="text" :value="currentValue" @input="handleInput" @blur="handleBlur">
+  <div :class="wrapClasses">
+    <input
+      :type="type"
+      :class="inputClasses"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :maxlength="maxlength"
+      :readonly="readonly"
+      :name="name"
+      :value="currentValue"
+      @input="handleInput"
+      @blur="handleBlur"
+      @focus="handleFocus"
+      @keyup.enter="handleEnter"
+    >
+  </div>
 </template>
 
 <script>
+import { oneOf } from '@/utils/assist.js'
 import Emitter from '@/mixins/emitter.js'
+const prefixCls = 'i-input'
 export default {
   name: 'iInput',
   mixins: [Emitter],
@@ -11,11 +28,66 @@ export default {
     value: {
       type: String,
       default: ''
+    },
+    type: {
+      validator (value) {
+        return oneOf(value, ['text', 'textarea', 'password', 'url', 'email', 'date', 'number', 'tel'])
+      },
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    maxlength: {
+      type: Number
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    name: {
+      type: String
+    },
+    number: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      validator (value) {
+        return oneOf(value, ['small', 'large', 'default'])
+      },
+      default: 'default'
     }
   },
   data () {
     return {
-      currentValue: this.value
+      currentValue: this.value,
+      prefixCls
+    }
+  },
+  computed: {
+    wrapClasses () {
+      return [
+        `${prefixCls}-wrapper`,
+        {
+          [`${prefixCls}-wrapper-${this.type}`]: !!this.size,
+          [`${prefixCls}-type`]: this.type
+        }
+      ]
+    },
+    inputClasses () {
+      return [
+        `${prefixCls}`,
+        {
+          [`${prefixCls}-${this.size}`]: !!this.size,
+          [`${prefixCls}-disabled`]: this.disabled
+        }
+      ]
     }
   },
   watch: {
@@ -29,13 +101,23 @@ export default {
       this.currentValue = value
       this.$emit('input', value) // input时间与prop value 构成双向绑定
       this.dispatch('iFormItem', 'on-form-change', value) // 有值改变，则通知form-item组件去校验
+
+      this.$emit('on-change', event)
     },
-    handleBlur () {
+    handleBlur (event) {
+      this.$emit('on-blur', event)
       this.dispatch('iFormItem', 'on-form-blur', this.currentValue)
+    },
+    handleFocus (event) {
+      this.$emit('on-focus', event)
+    },
+    handleEnter (event) {
+      this.$emit('on-enter', event)
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@/styles/components/input.scss";
 </style>
